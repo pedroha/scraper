dc55276422a88fb790ec85496e522bad994fd425
Pedro Ha <ph@educreational.com>
2016-07-26 07:51:01 -0400

    Fix children selector for lithuanian text

--- book2.js
+++ book2.js
@@ -8,15 +8,15 @@ var $ = require('cheerio')
 var request = require('request')
 var pictureTube = require('picture-tube')
 
-var downloadFile = function(url) {
-  	console.log('downloading...' + url)
+var downloadFile = function(url, folder) {
+	console.log('downloading...' + url)
   	
 	var idx = url.lastIndexOf('/')
 	if (idx > 0) {
 		var resourceName = url.substring(idx+1)
 		
 		console.log('downloading... ' + resourceName)
-		var stream = fs.createWriteStream('images/' + resourceName)
+		var stream = fs.createWriteStream(folder + '/' + resourceName)
 		request
 			.get(url)
 			.on('error', function(err) {
@@ -25,29 +25,6 @@ var downloadFile = function(url) {
 			.pipe(stream)
 		// working? Not really! Some bug in the pipe? or createStream
 	}
-}
-
-var DOMAIN = 'http://substack.net';
-
-function gotHTML(err, resp, html) {
-  if (err) return console.error(err)
-  var parsedHTML = $.load(html)
-  // get all img tags and loop over them
-  var imageURLs = []
-  parsedHTML('a').map(function(i, link) {
-    var href = $(link).attr('href')
-    if (!href.match('.png')) return
-    imageURLs.push(DOMAIN + href)
-  });
-
-  var randomIndex = Math.floor(Math.random() * imageURLs.length)
-  var randomImage = imageURLs[randomIndex]
-  console.log('Displaying... ' + randomImage)
-  request(randomImage).pipe(pictureTube()).pipe(process.stdout)	
-
-  // imageURLs.forEach(function(url) {
-  // 	downloadFile(url);
-  // })
 }
 
 var bookPathUrl = 'http://www.goethe-verlag.com/book2/EN/ENLT/';
@@ -66,22 +43,21 @@ var getTopicPage = function(err, resp, html) {
     var src = $audio.attr('src')
     console.log('adding... ' + src)
 
+    downloadFile(src, 'audio')
+
     var columns = $audio.parent().parent().siblings();
 
-    var english = $(columns[0]).text();
-    var lithuanian;
+    var english = $(columns[0]).text(); // This works!
+    var $lithuanian = $($(columns[1]).children()[0]).children();
 
-    $(columns[1]).children().each(function(i, child) {
-      if (i == 1) {
-        lithuanian = $(child).text();
-      }
-    })
+    var hint = $($lithuanian[0]).text();
+    var lithuanian = $($lithuanian[1]).text();
 
     console.log(english, lithuanian, src)
   })
 }
 
-var processTopic = function(relativeUrl) {
+var processTopic = function(topic, relativeUrl) {
   var url = bookPathUrl + relativeUrl;
   console.log(url);
   request(url, getTopicPage)
@@ -91,23 +67,37 @@ var getIndex = function(err, resp, html) {
   if (err) return console.error(err)
   var parsedHTML = $.load(html)
   var topicLinks = []
+  var topics = []
   // var sel = 'div.Styl39 > a'
   // var sel = 'div.col-md-4 > a'
   var sel = 'div.col-md-4 > div > a'
 
   parsedHTML(sel).each(function(i, link) {
     var href = $(link).attr('href')
-    // console.log('adding... ' + href)
+    var topic = $(link).text()
+    console.log('adding... ' + topic + ' : ' + href)
     topicLinks.push(href)
+    topics.push(topic)
   })
 
   // topicLinks.forEach(function(url) {
   //   processTopic(url)
   // })
 
-  processTopic(topicLinks[0]);
+  processTopic(topics[0], topicLinks[0]);
 }
 
 var bookIndexUrl = bookPathUrl + 'ENLT002.HTM';
-
 request(bookIndexUrl, getIndex)
+
+
+// fs.readFile('./ENLT002.HTM', function (err, data) {
+//   if (err) throw err
+//   console.log(data)
+
+//   getIndex(null, null, data)
+// });
+
+// For testing topic page
+// var html = fs.readFileSync('./ENLT003.HTM', 'utf8')
+// getTopicPage(null, null, html)
