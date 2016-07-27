94ad71cdc588a4262c888328064fda3b33d09861
Pedro Ha <ph@educreational.com>
2016-07-26 23:27:56 -0400

    Download all audio resources

--- book2.js
+++ book2.js
@@ -4,27 +4,46 @@
 // https://www.digitalocean.com/community/tutorials/how-to-use-node-js-request-and-cheerio-to-set-up-simple-web-scraping
 
 var fs = require('fs')
-var $ = require('cheerio')
 var request = require('request')
-var pictureTube = require('picture-tube')
+var $ = require('cheerio')
 
-var downloadFile = function(url, folder) {
-	console.log('downloading...' + url)
-  	
+var downloadFile = function(url, path) {
+    console.log('downloading... ' + url)
+
+    var stream = fs.createWriteStream(path)
+    request
+      .get(url)
+      .on('error', function(err) {
+        console.log(err)
+      })
+      .pipe(stream)
+};
+
+var downloadAudioResource = function(url, folder) {
 	var idx = url.lastIndexOf('/')
 	if (idx > 0) {
-		var resourceName = url.substring(idx+1)
-		
-		console.log('downloading... ' + resourceName)
-		var stream = fs.createWriteStream(folder + '/' + resourceName)
-		request
-			.get(url)
-			.on('error', function(err) {
-				console.log(err)
-			})
-			.pipe(stream)
-		// working? Not really! Some bug in the pipe? or createStream
-	}
+		var name = url.substring(idx+1)
+    var isAudioFile = (name && name.substring(name.length-4) === '.mp3');
+
+    if (isAudioFile) {
+      var path = folder + '/' + name; 
+      fs.access(path, fs.R_OK, (err) => {
+        if (err) { // We don't have it yet, so let's get it!
+          downloadFile(url, path)
+        }
+        else {
+          var stats = fs.statSync(path)
+          if (stats["size"] === 0) {
+            console.log('File size is 0: ' + name)
+            downloadFile(url, path)
+          }
+          else {
+            console.log('We already have this audio file: ' + name)
+          }
+        }
+      })
+    }
+  }
 }
 
 var bookPathUrl = 'http://www.goethe-verlag.com/book2/EN/ENLT/';
@@ -36,14 +55,14 @@ var getTopicPage = function(err, resp, html) {
   console.log('getTopicPage')  
   // var words = []
   // var sounds = []
-  
+
   var sel = 'audio > source'
   parsedHTML(sel).each(function(i, audio) {
     var $audio = $(audio)
     var src = $audio.attr('src')
     console.log('adding... ' + src)
 
-    downloadFile(src, 'audio')
+    downloadAudioResource(src, 'audio')
 
     var columns = $audio.parent().parent().siblings();
 
@@ -57,7 +76,7 @@ var getTopicPage = function(err, resp, html) {
   })
 }
 
-var processTopic = function(topic, relativeUrl) {
+var processTopic = function(relativeUrl) {
   var url = bookPathUrl + relativeUrl;
   console.log(url);
   request(url, getTopicPage)
@@ -80,12 +99,34 @@ var getIndex = function(err, resp, html) {
     topics.push(topic)
   })
 
-  // topicLinks.forEach(function(url) {
-  //   processTopic(url)
-  // })
-
-  processTopic(topics[0], topicLinks[0]);
+  topicLinks.forEach(function(url) {
+    processTopic(url)
+  })
+//  processTopic(topicLinks[0]);
 }
+
+var createFolder = function(path) {
+  fs.mkdir(path, function(err) {
+    if (err) {
+      console.error(err);
+    }
+    console.log("Directory created successfully!");
+  })
+}
+
+var ensureFolderExists = function(path) {
+  // Create /audio if it doesn't exist already
+  fs.readdir(path, function(err, files) {
+    if (err) {
+      createFolder(path)
+    }
+    files.forEach(function(file){
+      console.log( file );
+    });
+  })  
+}
+
+ensureFolderExists('audio');
 
 var bookIndexUrl = bookPathUrl + 'ENLT002.HTM';
 request(bookIndexUrl, getIndex)
