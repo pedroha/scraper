e012850d5ea250c59144f043920119a1fbe66e32
Pedro Ha <ph@educreational.com>
2016-07-30 00:56:07 -0400

    Create web page with all linked resources

--- book2-web.js
+++ book2-web.js
@@ -2,34 +2,46 @@ var hogan = require("hogan.js");
 var lithuanian = require('./lithuanian-uncompressed.json')
 //console.log(JSON.stringify(lithuanian))
 
-lithuanian = lithuanian.splice(0, 2)
+var audioBaseUrl = 'audio';
 
 // Convert Lithuanian to what we need with simple transforms
-
-// lithuanian = lithuanian.map(function(topic) {
-// });
-
-var baseUrl = 'audio';
-
+var cnt = 0;
 lithuanian.map(function(topic) {
 	topic.words.map(function(entry) {
-		entry.audio = baseUrl + '/' + entry.audio;
+		entry.id = 'audio-' + cnt;
+		entry.audio = audioBaseUrl + '/' + entry.audio;
+		cnt++;
 	});
 });
 
-console.log(JSON.stringify(lithuanian))
+// lithuanian = lithuanian.splice(0, 3)
+// console.log(JSON.stringify(lithuanian))
 
-var sectionTemplate = `
-<h3>{{topic}}</h3>
-<ul>
-{{#words}}
-	{{>entry}}
-{{/words}}
-</ul>
+var pageTemplate = `
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Labas!</title>
+</head>
+<body>
+{{{body}}}
+</body>
+</html>
 `;
 
-var entryTemplate = `
-<li>
+var sectionTemplate = 
+`
+	<h3>{{topic}}</h3>
+	<ul>
+	{{#words}}
+		{{>entry}}
+	{{/words}}
+	</ul>
+	<hr/>
+`;
+
+var entryTemplate = 
+`<li>
 	<div>{{english}}</div>
 	<div>{{lithuanian}}</div>
 	<audio id="{{id}}">
@@ -38,30 +50,12 @@ var entryTemplate = `
 </li>
 `;
 
-if (0) {
-	var topic = hogan.compile(sectionTemplate);
-	var rendered = topic.render(lithuanian[0], {entry: entryTemplate})
-	console.log(rendered)
-}
+var topicTpl = hogan.compile(sectionTemplate);
+var topics = lithuanian.map(function(topic) {
+	return topicTpl.render(topic, {entry: entryTemplate})
+})
 
+var pageTpl = hogan.compile(pageTemplate);
+var rendered = pageTpl.render({body: topics.join('')})
+console.log(rendered)
 
-/////////////////////////////////////////////////
-// var content = template.render( {
-// 	english: 'Anglais',
-// 	lithuanian: 'Hehehehe',
-// 	audio: 'someWave.mp3'
-// })
-
-// console.log(content)
-// var entry = lithuanian[0].words[0];
-// content = template.render(entry);
-// console.log(content)
-/////////////////////////////////////////////////
-
-if (0) {
-	var template = hogan.compile(entryTemplate);
-	var words = lithuanian[0].words.map(function(entry) {
-	  return template.render(entry)
-	})
-	console.log(words.join())
-}
