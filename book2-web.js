51cceb721fe1f2019e169154eaa8b91c1d6211d3
Pedro Ha <ph@educreational.com>
2016-07-31 23:19:52 -0400

    Support for utf-8 for the HTML page

--- book2-web.js
+++ book2-web.js
@@ -26,6 +26,7 @@ templates['page'] =
 <html>
 <head>
 	<title>Labas!</title>
+	<meta charset="utf-8" /> 
 	<link href='https://fonts.googleapis.com/css?family=Goudy+Bookletter+1911' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="${PROJECT_NAME}.css">
 </head>
